b8ee728a2e1468252251b4c95eadccf273734526
28604 <irvingchen930701@gmail.com>
2025-07-09 14:04:51 +0800

    Add Test Cases

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -13,18 +13,64 @@ func TestAdd(t *testing.T) {
 	// telling Go to run the test concurrently with other tests
 	// and make the testing process faster.
 	t.Parallel()
-	var want float64 = 4
-	got := calculator.Add(2, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+	// For different test cases, we create a testCase struct
+	// to store a pair of input and a desired output.
+	// So, rather than writing multiple tests, we write multiple test cases.
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: 1, b: 1, want: 2},
+		{a: 5, b: 0, want: 5},
+	}
+	for _, tc := range testCases {
+		got := calculator.Add(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Add(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
 
-func TestSubstract(t *testing.T) {
+func TestSubtract(t *testing.T) {
 	t.Parallel()
-	var want float64 = 2
-	got := calculator.Subtract(4, 2)
-	if want != got {
-		t.Errorf("want %f, got %f", want, got)
+
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 4, b: 2, want: 2},
+		{a: 6, b: 1, want: 5},
+		{a: -1, b: -3, want: 2},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Subtract(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Subtract(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		a, b float64
+		want float64
+	}
+	testCases := []testCase{
+		{a: 2, b: 2, want: 4},
+		{a: -2, b: 3, want: -6},
+		{a: 0, b: 10, want: 0},
+	}
+
+	for _, tc := range testCases {
+		got := calculator.Multiply(tc.a, tc.b)
+		if tc.want != got {
+			t.Errorf("Multiply(%f, %f): want %f, got %f", tc.a, tc.b, tc.want, got)
+		}
 	}
 }
