030cb581eec45b5ed5fc194291453fd707857388
28604 <irvingchen930701@gmail.com>
2025-07-09 22:47:55 +0800

    Add Buy

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,9 +1,19 @@
 package bookstore
 
+import "errors"
+
 // To use the struct Book outside of the "bookstore" package,
 // we must capitalised the first letter of the struct "Book".
 type Book struct {
 	Title  string
 	Author string
 	Copies int
+}
+
+func Buy(b Book) (Book, error) {
+	if b.Copies == 0 {
+		return Book{}, errors.New("no copies left")
+	}
+	b.Copies--
+	return b, nil
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -1,3 +1,13 @@
+// To check the coverage of tests (coverage of the system code)
+// $ go test -cover
+
+// To see what lines are covered and what aren't.
+// $ go test -coverprofile=coverage.out
+// $ go tool cover -html=coverage.out
+
+// Make sure the coverage is as high as possible before moving on.
+// Check if every behavior is correct.
+
 package bookstore_test
 
 import (
@@ -12,5 +22,36 @@ func TestBook(t *testing.T) {
 		Title:  "Spark Joy",
 		Author: "Marie Condo",
 		Copies: 2,
+	}
+}
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Title 1",
+		Author: "John Doe",
+		Copies: 3,
+	}
+	want := 2
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestBuyErrorsIfNoCopiesLeft(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Title 2",
+		Author: "James Doe",
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Error("Want error buying from zero copies, got nil.")
 	}
 }
